967bd8ee372dc30df67eb59da9cea1c652d93f8c
macjikkk <mcjikblackstar@gmail.com>
2023-06-20 17:34:23 +0500

    homepage change

--- server.js
+++ server.js
@@ -16,24 +16,3 @@ app.get("/", (req, res) => {
 });
 
 console.log('bro')
-
-// Import the functions you need from the SDKs you need
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyANMX_kCpYJu9N7DrzSf9bu7sf9xWjGu0M",
-  authDomain: "final-project-backend-3c767.firebaseapp.com",
-  projectId: "final-project-backend-3c767",
-  storageBucket: "final-project-backend-3c767.appspot.com",
-  messagingSenderId: "370891920659",
-  appId: "1:370891920659:web:ce0abe045377f0220176e6",
-  measurementId: "G-TD9QXVZRPJ"
-};
-
-// Initialize Firebase
-const initializeApp = initializeApp(firebaseConfig);
-const analytics = getAnalytics(initializeApp);
-console.log(analytics)
