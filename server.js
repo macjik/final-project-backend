40d2820e30071ef41dc0033667256c6c07923a7d
macjikkk <mcjikblackstar@gmail.com>
2023-06-20 16:44:05 +0500

    firebase integration

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { initializeApp } =require( "firebase/app");
+const { getAnalytics } = require("firebase/analytics");
 const app = express();
 
 app.listen(3001, () => {
@@ -11,24 +13,23 @@ app.get("/", (req, res) => {
 
 console.log('bro')
 
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyANMX_kCpYJu9N7DrzSf9bu7sf9xWjGu0M",
-//   authDomain: "final-project-backend-3c767.firebaseapp.com",
-//   projectId: "final-project-backend-3c767",
-//   storageBucket: "final-project-backend-3c767.appspot.com",
-//   messagingSenderId: "370891920659",
-//   appId: "1:370891920659:web:ce0abe045377f0220176e6",
-//   measurementId: "G-TD9QXVZRPJ"
-// };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyANMX_kCpYJu9N7DrzSf9bu7sf9xWjGu0M",
+  authDomain: "final-project-backend-3c767.firebaseapp.com",
+  projectId: "final-project-backend-3c767",
+  storageBucket: "final-project-backend-3c767.appspot.com",
+  messagingSenderId: "370891920659",
+  appId: "1:370891920659:web:ce0abe045377f0220176e6",
+  measurementId: "G-TD9QXVZRPJ"
+};
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+// Initialize Firebase
+const initializeApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(initializeApp);
+console.log(analytics)
