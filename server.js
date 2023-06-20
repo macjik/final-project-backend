90371cdb7ad76f096fa7e13979bb2f59a39fc680
macjikkk <mcjikblackstar@gmail.com>
2023-06-20 17:01:41 +0500

    firebase delete

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const { initializeApp } =require( "firebase/app");
 const { getAnalytics } = require("firebase/analytics");
+
 const app = express();
+app.use(express.json())
+app.use(cors())
 
 app.listen(3001, () => {
   console.log("yehhh");
