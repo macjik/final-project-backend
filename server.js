98fac47e52d03bcfb033f32351c2939cda39c872
macjikkk <mcjikblackstar@gmail.com>
2023-07-05 22:33:18 +0500

    connecting mongo db

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { initializeApp } =require( "firebase/app");
-const { getAnalytics } = require("firebase/analytics");
+const mongoose = require("mongoose");
 
 const app = express();
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 app.listen(3001, () => {
   console.log("yehhh");
@@ -15,4 +15,25 @@ app.get("/", (req, res) => {
   res.send("Wagwan G");
 });
 
-console.log('bro')
+console.log("bro");
+
+mongoose.connect(process.env.MONGO_DB, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const User = mongoose.model("Users", UserSchema);
+
+module.exports = User;
