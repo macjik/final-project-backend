aca4bee94870e6345540ad0e98a530b1f0dbbeea
macjikkk <mcjikblackstar@gmail.com>
2023-07-05 22:51:50 +0500

    setting env for port

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
   console.log("yehhh");
 });
 
