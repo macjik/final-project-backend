818e902dc414244535ba6aa4cea2c1b572a63a13
macjikkk <mcjikblackstar@gmail.com>
2023-07-07 10:57:54 +0500

    fixing changes

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3120;
 
-const app = express();
-app.use(express.json());
 app.use(cors());
-
-app.listen(PORT, () => {
-  console.log("yehhh");
-});
-
-console.log("bro");
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_DB, {
@@ -44,22 +38,28 @@ const user = new User({
   age: 45,
 });
 
-app.get("/", async (req, res) => {
-  try {
-    console.log(req);
-    res.json(req);
-    await user.save();
-  } catch (error) {
-    if (error.code === 11000) {
-      console.log("Duplicate name found. Ignoring the error.");
-    } else {
-      console.log("An error occurred while saving the user:", error);
-    }
-  }
+app.post("/", (req, res) => {
+  // res.send("ass");
+  res.send(req.body);
+  console.log(req.body);
+
+  // try {
+  //   await user.save();
+  // } catch (error) {
+  //   if (error.code === 11000) {
+  //     console.log("Duplicate name found. Ignoring the error.");
+  //   } else {
+  //     console.log("An error occurred while saving the user:", error);
+  //   }
+  // }
 });
 
 // app.get("http://localhost:3000/register", (req, res) => {
 //   console.log(req);
 // });
+
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
 
 module.exports = app;
