35c32817dbc9a57243ea392a49d68ece62a6405e
macjikkk <mcjikblackstar@gmail.com>
2023-07-09 16:09:07 +0500

    authentification

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookie = require("cookie");
 
 const PORT = 3120;
 
@@ -31,39 +35,90 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-UserSchema.index({ username: 1 }, { unique: true });
-
 const User = mongoose.model("Users", UserSchema);
 
-app.post("/", async (req, res) => {
-  // res.send("ass");
-  res.send(req.body);
-  console.log(req.body);
+// UserSchema.index({ username: 1 }, { unique: true });
+app.post(
+  "/",
+  [
+    body("userData.username").notEmpty().withMessage("Username is required"),
+    body("userData.email").isEmail().withMessage("Invalid email address"),
+    body("userData.password")
+      .notEmpty()
+      .withMessage("Password is required")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+  ],
+  async (req, res) => {
+    // res.send("ass");
+    // res.send(req.body);
+    console.log(req.body);
 
-  const user = new User({
-    username: req.body.userData.username,
-    email: req.body.userData.email,
-    password: req.body.userData.password,
-  });
+    const user = new User({
+      username: req.body.userData.username,
+      email: req.body.userData.email,
+      password: await bcrypt.hash(req.body.userData.password, 10),
+    });
+
+    try {
+      await user.save();
+      res.send(user);
+    } catch (error) {
+      if (error.code === 11000) {
+        return res.status(400).send("Duplicate name found");
+      } else {
+        return res.status(500).send("An error occurred while saving the user");
+      }
+    }
+    console.log(user);
+  }
+);
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body.userData;
 
   try {
-    await user.save();
-  } catch (error) {
-    if (error.code === 11000) {
-      console.log("Duplicate name found. Ignoring the error.");
-    } else {
-      console.log("An error occurred while saving the user:", error);
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid username or password" });
     }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ message: "Invalid username or password" });
+    }
+
+    const payload = {
+      userId: user._id,
+      username: user.username,
+    };
+
+    const secretKey = "your_secret_key";
+    const expiresIn = "7d";
+
+    const token = jwt.sign(payload, secretKey, { expiresIn });
+
+    // Set the token as a cookie
+    res.setHeader(
+      "Set-Cookie",
+      cookie.serialize("token", token, {
+        httpOnly: true,
+        secure: true, // Set to true if using HTTPS
+        sameSite: "strict",
+        maxAge: 10604800, // Expiration time in seconds (7 days)
+        path: "http://localhost:3000/login", // Adjust the path if needed
+      })
+    );
+
+    res.json({ token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error" });
   }
-  console.log(user)
 });
 
-// app.get("http://localhost:3000/register", (req, res) => {
-//   console.log(req);
-// });
-
 app.listen(PORT, () => {
-  console.log(`listening on ${PORT}`);
+  console.log(`Listening on ${PORT}`);
 });
 
 module.exports = app;
