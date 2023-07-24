7231548bad147ec40e7b2fe60de8e9035d09b014
macjikkk <mcjikblackstar@gmail.com>
2023-07-25 01:33:01 +0500

    optimizing

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const cookie = require("cookie");
-// const multer = require("multer");
 
 const PORT = 3120;
 
@@ -205,35 +204,10 @@ app.post("/home-collections", async (req, res) => {
   }
 });
 
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "uploads/"); // Specify the directory where the images will be saved
-//   },
-//   filename: function (req, file, cb) {
-//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-//     cb(
-//       null,
-//       file.fieldname +
-//         "-" +
-//         uniqueSuffix +
-//         "." +
-//         file.originalname.split(".").pop()
-//     );
-//   },
-// });
-
-// const upload = multer({ storage: storage });
-
-// app.post("/imageUpload", upload.single("content"), (req, res) => {
-//   try {
-//     const imageUrl = req.file.path;
-//     console.log(imageUrl);
-//     res.json({ imageUrl });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: "Image upload failed" });
-//   }
-// });
+// app.post('/imageUpload', async (req, res) => {
+//   console.log(req.body)
+//   await res.send(req.body)
+// })
 
 console.log(collections);
 
