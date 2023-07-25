b97992240a4e27e18ca1b22b761347ae9d79a1d3
macjikkk <mcjikblackstar@gmail.com>
2023-07-25 17:28:16 +0500

    setting headers

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ const PORT = 3120;
 app.use(express.json());
 app.use(cors());
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 mongoose
   .connect(process.env.MONGO_DB, {
