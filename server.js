3d3f790906cc1072c610f5a9aedd28047ed684e9
macjikkk <mcjikblackstar@gmail.com>
2023-07-25 14:58:15 +0500

    multer storage fix

--- server.js
+++ server.js
@@ -214,7 +214,7 @@ app.post("/home-collections", async (req, res) => {
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, "upload/");
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
