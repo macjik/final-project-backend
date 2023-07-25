f853a65de9ef37193b988913aa92ec34872ef1c1
macjikkk <mcjikblackstar@gmail.com>
2023-07-25 14:30:19 +0500

    readding app headers

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ const PORT = 3120;
 app.use(express.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 mongoose
   .connect(process.env.MONGO_DB, {
     useNewUrlParser: true,
