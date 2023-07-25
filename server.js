50ba4d05e8e5b704c112ec0dd33fe4fed1ccdd64
macjikkk <mcjikblackstar@gmail.com>
2023-07-25 14:34:08 +0500

    setting headers

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(cors());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
