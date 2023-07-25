b8474dc2d96afe43fb4045f50505473cf5604c31
macjikkk <mcjikblackstar@gmail.com>
2023-07-25 14:11:42 +0500

    server port change

--- server.js
+++ server.js
@@ -225,7 +225,7 @@ app.post("/upload", upload.single("content"), (req, res) => {
   fileStream.pipe(res);
 });
 
-app.listen(3000, () => {
+app.listen(3120, () => {
   console.log("Server is running on port 3000");
 });
 
